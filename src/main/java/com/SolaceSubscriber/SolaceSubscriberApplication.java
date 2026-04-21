d32b2e42735374e447273ed8c8797a419a52a101
Shivamkharade <shivamkharade189@gmail.com>
2026-04-21 16:42:44 +0530

    intial commit

---
+++ src/main/java/com/SolaceSubscriber/SolaceSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.SolaceSubscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceSubscriberApplication.class, args);
+	}
+
+}
