3aaed71f59fd975fafd306630cc37e6d655252c3
Shivamkharade <shivamkharade189@gmail.com>
2026-04-21 17:42:11 +0530

    i think its done

--- src/main/java/com/SolaceSubscriber/config/EventListener.java
+++ src/main/java/com/SolaceSubscriber/config/EventListener.java
@@ -5,16 +5,24 @@ import java.util.function.Consumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import com.SolaceSubscriber.Services.EmailService;
 import com.SolaceSubscriber.event.UserLoginEvent;
 
 @Component
 public class EventListener {
+	
+	private final EmailService emailService;
+	
+	public EventListener(EmailService emailService1) {
+		this.emailService = emailService1;
+	}
 	
 	@Bean
 	public Consumer<UserLoginEvent> userLogin(){
 		
 		return event ->{
 			System.out.println("Recived Event : " + event.getEmail());
+			emailService.sendEmail(event.getEmail());
 		};
 	}
 	
