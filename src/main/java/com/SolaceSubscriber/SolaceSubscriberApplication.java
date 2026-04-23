03ae125b30a67d11b336d49b3c29ff1460e2498b
Shivamkharade <shivamkharade189@gmail.com>
2026-04-23 14:21:23 +0530

    configured the env file

--- src/main/java/com/SolaceSubscriber/SolaceSubscriberApplication.java
+++ src/main/java/com/SolaceSubscriber/SolaceSubscriberApplication.java
@@ -10,7 +10,6 @@ public class SolaceSubscriberApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-
 	    System.setProperty("MAIL", dotenv.get("MAIL"));
 	    System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 		SpringApplication.run(SolaceSubscriberApplication.class, args);
